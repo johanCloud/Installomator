32e23601369272b2672ee04b93c8965e9e36b4f7
Søren Theilgaard <soren@theilgaard.dk>
2021-04-12 22:23:11 +0200

    v0.5.1

--- CheckInstallomator.sh
+++ CheckInstallomator.sh
@@ -25,7 +25,7 @@ ${SELFLOCATION}/Installomator.sh longversion
 echo
 
 # Labels with the arch call for different versions for Intel and Apple Silicon should be listed here:
-archLabels=( boxdrive brave googlechrome jetbrainspycharm notion vlc zulujdk11 zulujdk13 zulujdk15 )
+archLabels=( boxdrive brave googlechrome jetbrainspycharm jetbrainspycharmce pycharmce notion vlc zulujdk11 zulujdk13 zulujdk15 )
 
 # MARK: check minimal macOS requirement
 autoload is-at-least
