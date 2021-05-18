8a9252b9a116a130ba88728de0737a2d2752e7cb
Søren Theilgaard <soren@theilgaard.dk>
2021-05-18 20:58:13 +0200

    v0.5.5

--- InstallomatorLabels.sh
+++ InstallomatorLabels.sh
@@ -585,13 +585,19 @@ firefox_da)
     blockingProcesses=( firefox )
     ;;
 firefox_intl)
+    # This label will try to figure out the selected language of the user, 
+    # and install corrosponding version of Firefox
     name="Firefox"
     type="dmg"
-    userLanguage=$(runAsUser defaults read .GlobalPreferences AppleLocale | cut -c 1-2)
+    userLanguage=$(runAsUser defaults read .GlobalPreferences AppleLocale)
     printlog "Found language $userLanguage to be used for Firefox."
-    if [[ "$userLanguage" == "en" ]]; then
-        userLanguage="en_US"
+    if ! curl -fs "https://ftp.mozilla.org/pub/firefox/releases/latest/README.txt" | grep -o "=$userLanguage"; then
+        userLanguage=$(echo $userLanguage | cut -c 1-2)
+        if ! curl -fs "https://ftp.mozilla.org/pub/firefox/releases/latest/README.txt" | grep "=$userLanguage"; then
+            userLanguage="en_US"
+        fi
     fi
+    printlog "Using language $userLanguage for download."
     downloadURL="https://download.mozilla.org/?product=firefox-latest&amp;os=osx&amp;lang=$userLanguage"
     if ! curl -sfL --output /dev/null -r 0-0 "$downloadURL" ; then
         printlog "Download not found for that language. Using en-US"
@@ -611,13 +617,19 @@ firefoxesrpkg)
     blockingProcesses=( firefox )
     ;;
 firefoxesr_intl)
+    # This label will try to figure out the selected language of the user, 
+    # and install corrosponding version of Firefox ESR
     name="Firefox"
     type="dmg"
-    userLanguage=$(runAsUser defaults read .GlobalPreferences AppleLocale | cut -c 1-2)
+    userLanguage=$(runAsUser defaults read .GlobalPreferences AppleLocale)
     printlog "Found language $userLanguage to be used for Firefox."
-    if [[ "$userLanguage" == "en" ]]; then
-        userLanguage="en_US"
+    if ! curl -fs "https://ftp.mozilla.org/pub/firefox/releases/latest-esr/README.txt" | grep -o "=$userLanguage"; then
+        userLanguage=$(echo $userLanguage | cut -c 1-2)
+        if ! curl -fs "https://ftp.mozilla.org/pub/firefox/releases/latest-esr/README.txt" | grep "=$userLanguage"; then
+            userLanguage="en_US"
+        fi
     fi
+    printlog "Using language $userLanguage for download."
     downloadURL="https://download.mozilla.org/?product=firefox-esr-latest-ssl&os=osx&lang=$userLanguage"
     # https://download.mozilla.org/?product=firefox-esr-latest-ssl&os=osx&lang=en-US
     if ! curl -sfL --output /dev/null -r 0-0 "$downloadURL" ; then
