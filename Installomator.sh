8dbf7db1bacbc35d9070071d9f4b4a2ff98f7242
Søren Theilgaard <soren@theilgaard.dk>
2021-06-11 10:01:57 +0200

    v0.5.8

--- Installomator.sh
+++ Installomator.sh
@@ -21,7 +21,7 @@
 #set -x # Debug
 
 VERSION='0.5.8' # This version branched by Søren Theilgaard
-VERSIONDATE='2021-0?-??'
+VERSIONDATE='2021-06-??'
 VERSIONBRANCH='Søren Theilgaard'
 
 export PATH=/usr/bin:/bin:/usr/sbin:/sbin
