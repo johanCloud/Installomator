a4c9d287bdad0b6781ca5e8a2fb5ee81c2f53edc
Søren Theilgaard <soren@theilgaard.dk>
2021-07-01 14:50:34 +0200

    v0.5.9
    4kvideodownloader and md5 for 0.5.8 added

--- InstallomatorLabels.sh
+++ InstallomatorLabels.sh
@@ -35,6 +35,13 @@ longversion)
     #forcefulQuit=YES
     #Company="Agilebits"
     ;;
+4kvideodownloader)
+    name="4K Video Downloader"
+    type="dmg"
+    downloadURL="$(curl -fsL "https://www.4kdownload.com/products/product-videodownloader" | grep -E -o "https:\/\/dl\.4kdownload\.com\/app\/4kvideodownloader_.*?.dmg\?source=website" | head -1)"
+    appNewVersion=$(echo "${downloadURL}" | sed -E 's/.*\/[0-9a-zA-Z]*_([0-9.]*)\.dmg.*/\1/g')
+    expectedTeamID="GHQ37VJF83"
+    ;;
 8x8)
     # credit: #D-A-James from MacAdmins Slack and Isaac Ordonez, Mann consulting (@mannconsulting)
     name="8x8 Work"
