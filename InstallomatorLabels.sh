afe662d20b1d11facbda6aa878bab37459cd19a4
Søren Theilgaard <soren@theilgaard.dk>
2021-09-10 23:06:05 +0200

    v0.6.1

--- InstallomatorLabels.sh
+++ InstallomatorLabels.sh
@@ -255,6 +255,13 @@ audacity)
     appNewVersion=$(versionFromGit audacity audacity)
     expectedTeamID="T3N4JQ7YY6"
     ;;
+authydesktop)
+    name="Authy Desktop"
+    type="dmg"
+    downloadURL="https://electron.authy.com/download?channel=stable&arch=x64&platform=darwin&version=latest&product=authy"
+    appNewVersion="$(curl -sfL --output /dev/null -r 0-0 "${downloadURL}" --remote-header-name --remote-name -w "%{url_effective}\n" | grep -o -E '([a-zA-Z0-9\_.%-]*)\.(dmg|pkg|zip|tbz)$' | sed -E 's/.*-([0-9.]*)\.dmg/\1/g')"
+    expectedTeamID="9EVH78F4V4"
+    ;;
 autodmg)
     # credit: Mischa van der Bent (@mischavdbent)
     name="AutoDMG"
